f6c40560647d1bbc405e132cc77bbeab3dac346b
Steven Gilvarry <steve.gilvarry@gmail.com>
2021-02-07 21:15:11 +1100

    Add hls base

---
+++ src/zm_hls.h
@@ -0,0 +1,14 @@
+//
+// Created by Steven Gilvarry on 28/7/20.
+//
+
+#ifndef ZONEMINDER_ZM_HLS_H
+#define ZONEMINDER_ZM_HLS_H
+
+class zm_hls {
+ public:
+  zm_hls();
+  virtual ~zm_hls();
+};
+
+#endif //ZONEMINDER_ZM_HLS_H
